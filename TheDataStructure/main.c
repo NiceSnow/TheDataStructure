dc013416a6fb070e0aef756ff04b1e259165d252
madongdong <mddios2012@163.com>
2020-11-08 18:11:12 +0800

    Initial Commit

---
+++ TheDataStructure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TheDataStructure
+//
+//  Created by madongdong on 2020/11/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
