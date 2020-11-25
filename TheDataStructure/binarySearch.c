3c43c45e8cb50d1eec57eab70774c8dc103affd9
madongdong <mddios2012@163.com>
2020-11-25 19:52:48 +0800

    二分查找
    二分查找

--- TheDataStructure/binarySearch.c
+++ TheDataStructure/binarySearch.c
@@ -22,3 +22,75 @@ int binarySearch (int* arr,int low, int high ,int val) {
         return binarySearch(arr, low, mid - 1,val);
     }
 }
+
+//第一个值等于给定值的元素。
+
+int bsearch4(int* a, int n, int value) {
+  int low = 0;
+  int high = n - 1;
+  while (low <= high) {
+    int mid =  low + ((high - low) >> 1);
+    if (a[mid] > value) {
+      high = mid - 1;
+    } else if (a[mid] < value) {
+      low = mid + 1;
+    } else {
+      if ((mid == 0) || (a[mid - 1] != value)) return mid;
+      else high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+//最后一个值等于给定值
+
+int bsearch5(int* a, int n, int value) {
+  int low = 0;
+  int high = n - 1;
+  while (low <= high) {
+    int mid =  low + ((high - low) >> 1);
+    if (a[mid] > value) {
+      high = mid - 1;
+    } else if (a[mid] < value) {
+      low = mid + 1;
+    } else {
+      if ((mid == n - 1) || (a[mid + 1] != value)) return mid;
+      else low = mid + 1;
+    }
+  }
+  return -1;
+}
+
+//第一个大于等于给定值的元素
+
+int bsearch6(int* a, int n, int value) {
+  int low = 0;
+  int high = n - 1;
+  while (low <= high) {
+    int mid =  low + ((high - low) >> 1);
+    if (a[mid] >= value) {
+      if ((mid == 0) || (a[mid - 1] < value)) return mid;
+      else high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return -1;
+}
+
+//最后一个小于等于给定值的元素
+
+int bsearch7(int* a, int n, int value) {
+  int low = 0;
+  int high = n - 1;
+  while (low <= high) {
+    int mid =  low + ((high - low) >> 1);
+    if (a[mid] > value) {
+      high = mid - 1;
+    } else {
+      if ((mid == n - 1) || (a[mid + 1] > value)) return mid;
+      else low = mid + 1;
+    }
+  }
+  return -1;
+}
